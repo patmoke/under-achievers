bb99e1dac53c4125ea679c6cf774813b606238c8
patmoke <patrickrmoke@gmail.com>
2025-09-13 16:19:22 -0400

    update serve2

--- src/lib/supabaseServer.ts
+++ src/lib/supabaseServer.ts
@@ -1,8 +1,15 @@
 import { createServerClient } from '@supabase/ssr';
 
+// dummy cookie methods (not actually used in cron jobs)
+const serverCookies = {
+  get: (_name: string) => undefined,
+  set: (_name: string, _value: string) => {},
+  delete: (_name: string) => {},
+};
+
 export const supabaseServer = () =>
   createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.SUPABASE_SERVICE_ROLE_KEY!,
-    {} // object required
+    { cookies: serverCookies }
   );
