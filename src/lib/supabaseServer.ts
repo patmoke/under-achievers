181bf64381c5c6da9eefa7a6a77bbe4d7473bae6
patmoke <patrickrmoke@gmail.com>
2025-09-13 16:11:30 -0400

    update server

--- src/lib/supabaseServer.ts
+++ src/lib/supabaseServer.ts
@@ -1,10 +1,20 @@
-import { cookies } from 'next/headers';
+import { cookies as nextCookies } from 'next/headers';
 import { createServerClient } from '@supabase/ssr';
 
-export function supabaseServer() {
+export const supabaseServer = () => {
+  const cookieMethods = {
+    get: (name: string) => nextCookies().get(name),
+    set: (name: string, value: string) => {
+      nextCookies().set(name, value);
+    },
+    delete: (name: string) => {
+      nextCookies().delete(name);
+    },
+  };
+
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    { cookies }
+    { cookies: cookieMethods }
   );
-}
+};
